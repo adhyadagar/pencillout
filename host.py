4282c46c57aece144e34adcbc6d0fcab768e0dcf
Adhya Dagar <adagz9816@gmail.com>
2024-06-14 02:06:58 -0700

    Add files via upload

--- host.py
+++ host.py
@@ -5,7 +5,9 @@ from conplex import ConplexModel  # This assumes you need the ConplexModel for p
 app = Flask(__name__)
 
 # Load the saved model
-model = joblib.load('antibacterial_model.joblib')
+model_antibacterial = joblib.load('antibacterial_model.joblib')
+model_antitumour    = joblib.load('antitumour_model.joblib')
+toxicity_model = joblib.load('toxicity_model.joblib')
 
 # Assume ConplexModel is used to generate embeddings
 embedding_model = ConplexModel()
@@ -28,13 +30,22 @@ def predict():
         # Generate embedding
         # embedding = embedding_model.embed([smiles_string])  # Assuming embed needs a list
         # embedding = np.array([embedding])  # Ensure it is in the correct format
-        prediction = model.predict([smiles_string])  # Get probability of the positive class
+        prediction_antibacterial = model_antibacterial.predict([smiles_string])  # Get probability of the positive class
+        prediction_antibacterial = prediction_antibacterial.tolist()  # Convert to list if it's not already
+        print("Antibacterial prediction:", prediction_antibacterial)
+        prediction_antitumour = model_antitumour.predict([smiles_string])  # Get probability of the positive class
+        prediction_antitumour = prediction_antitumour.tolist()  # Convert to list if it's not already
+        print("Antitumour prediction:", prediction_antitumour)  
+        prediction = toxicity_model.predict([smiles_string])  # Get probability of the positive class
         prediction = prediction.tolist()  # Convert to list if it's not already
-        print("Prediction:", prediction)
+        print("Toxicity prediction:", prediction)
+
 
         # Convert prediction to string if necessary
+        prediction_antibacterial_str = str(prediction_antibacterial[0])
+        prediction_antitumour_str = str(prediction_antitumour[0])
         prediction_str = str(prediction[0])
-        return jsonify({'probability': prediction_str})
+        return jsonify({'antibacterial': prediction_antibacterial_str, 'antitumour': prediction_antitumour_str, 'toxicity': prediction_str })
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
